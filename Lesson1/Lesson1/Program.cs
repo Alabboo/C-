2ef7d4acdfed5ae95c0005282eabb6a9f41962e3
Alabboo <hanafyjonas@gmail.com>
2023-12-06 14:33:14 +0100

    Lesson 1 and Lesson 2

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Congratulations");
+//Console.WriteLine(" ");
+//Console.WriteLine("You wrote your first lines of code");
+//
+//Console.WriteLine("This is the first line.");
+//Console.WriteLine("This is the second line.");
