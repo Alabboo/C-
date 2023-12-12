11fafe248c44ed2b914a320cc6e3602a882f6326
Alabboo <hanafyjonas@gmail.com>
2023-12-12 09:54:30 +0100

    Lesson 3

--- Lesson3/Lesson3/obj/Debug/net8.0/Lesson3.AssemblyInfo.cs
+++ Lesson3/Lesson3/obj/Debug/net8.0/Lesson3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770fe61bf366ba435c473e4d06b302e6dc25d33e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
